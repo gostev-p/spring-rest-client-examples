2a50bf4a917b89cbca67c059efb4819b8f31c8c4
Gostev, Philipp <philipp.gostev@adesso.de>
2023-03-23 10:03:58 +0100

    refactor to use reactive

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -1,8 +1,14 @@
 package guru.springframework.springrestclientexamples.services;
 
 import guru.springframework.springrestclientexamples.api.domain.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -10,13 +16,34 @@ import java.util.List;
 public class ApiServiceImpl implements ApiService {
 
     private final RestTemplate restTemplate;
+    private final String api_url;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api_url}") String apiUrl) {
         this.restTemplate = restTemplate;
+        api_url = apiUrl;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
-        return restTemplate.getForObject("https://jsonplaceholder.typicode.com/users?_limit=" + limit, List.class);
+
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder
+                .fromUriString(api_url)
+                .queryParam("_limit", limit);
+
+        List<User> userData = restTemplate.getForObject(uriComponentsBuilder.toUriString(), List.class);
+
+        return userData;
+    }
+
+    @Override
+    public Flux<User> getUsers(Mono<Integer> limit) {
+        return WebClient
+                .create(api_url)
+                .get()
+                .uri(uriBuilder -> uriBuilder.queryParam("_limit", limit.block()).build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .flatMap(clientResponse -> clientResponse.bodyToMono(List.class))
+                .flatMapIterable(list -> list);
     }
 }
